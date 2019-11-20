d2463c97c1ab8f67fbbe9081bb2bfca095ffb767
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-20 08:43:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,5 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  nikki = ["Mahal ko", "Baby-shi", "Gai-sensei", "Malabs", "arrays!"]
+  nikki[-1]
 end
