56c61eb76a5dba3ae7542e923af49ff43c164477
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-20 08:32:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  nikki = ["Mahal ko", "Baby-shi", "Gai-sensei", "Malabs", "arrays!"]
+  nikki.pop "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
