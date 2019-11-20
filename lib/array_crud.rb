03708fbe11b6839538c158eb5172c2508a6c9efc
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-20 08:42:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  nikki = ["wow", "Mahal ko", "Baby-shi", "Gai-sensei", "Malabs"]
+  nikki[0]
 end
 
 def retrieve_last_element_from_array(array)
