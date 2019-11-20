d108b7d2feecc6c5f3db4a7a859a04340a841c8c
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-20 08:39:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  nikki = ["wow", "Mahal ko", "Baby-shi", "Gai-sensei", "Malabs"]
+  array = nikki
 end
 
 def retrieve_element_from_index(array, index_number)
